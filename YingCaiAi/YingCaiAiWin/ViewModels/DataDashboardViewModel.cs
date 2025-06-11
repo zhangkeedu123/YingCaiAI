dfe4362491a6e6714d64aa2199464d901cae9145
zhangkeedu123 <zhangkeedu@163.com>
2025-06-11 22:44:10 +0800

    1

--- YingCaiAi/YingCaiAiWin/ViewModels/DataDashboardViewModel.cs
+++ YingCaiAi/YingCaiAiWin/ViewModels/DataDashboardViewModel.cs
@@ -77,7 +77,7 @@ namespace YingCaiAiWin.ViewModels
 
           
             _timer = new DispatcherTimer();
-            _timer.Interval = TimeSpan.FromSeconds(4); 
+            _timer.Interval = TimeSpan.FromSeconds(3); 
             _timer.Tick += async (s, e) => await RefreshData();
             _timer.Start();
             InitializeData();
